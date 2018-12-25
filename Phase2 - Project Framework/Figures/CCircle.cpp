7eb3ef93f671a9fc45aecc6acec8f98b80876819
mzaki97 <32806958+mzaki97@users.noreply.github.com>
2018-12-25 19:48:28 +0200

    Update CCircle.cpp

--- Phase2 - Project Framework/Figures/CCircle.cpp
+++ Phase2 - Project Framework/Figures/CCircle.cpp
@@ -14,9 +14,11 @@ void CCircle::getCenter(Point & p1)const {
 }
 
 void CCircle::Draw(Output * pOut)const {
-
-	pOut->DrawCir(center, FigGfxInfo, IsCut, Selected, factor);
-	pOut->DrawOrigin(center);
+	if (!Hidden)
+	{
+		pOut->DrawCir(center, FigGfxInfo, Selected, factor);
+		pOut->DrawOrigin(center);
+	}
 }
 bool CCircle::isInside(Point click){
 	float D = getDistance(click, center);
@@ -27,4 +29,12 @@ bool CCircle::isInside(Point click){
 
 
 
+}
+bool CCircle::sametype(CFigure* p)
+{
+	return (dynamic_cast<CCircle*>(p) != NULL);
+}
+string CCircle::String()
+{
+	return "Circle";
 }
